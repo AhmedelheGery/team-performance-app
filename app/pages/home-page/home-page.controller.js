a83ed9ba9e18a566e366d5b3f608257a7639fd9b
Ahmed Alaa <ahmed.3laa.2020@gmail.com>
2022-06-12 10:32:01 +0200

    feat(team_table): implement highlight filter for team table

--- app/pages/home-page/home-page.controller.js
+++ app/pages/home-page/home-page.controller.js
@@ -2,12 +2,18 @@ angular
   .module('appModule')
   .controller('homeController', homePageController);
 
-function homePageController(Employees) {
+function homePageController(Employees, $location) {
   const homePageVm = this;
   homePageVm.employees = [];
-
+  homePageVm.queryStringValue = '';
+  homePageVm.handleFilterEvent = (employees, queryString) => {
+    homePageVm.employees = employees;
+    return queryString ? $location.search('filter', queryString) : $location.search({});
+  };
+  if ($location.search().filter) {
+    homePageVm.filterQueryString = $location.search().filter;
+  }
   activate();
-
   function activate() {
     Employees.getEmployees()
       .then(({ data }) => {
