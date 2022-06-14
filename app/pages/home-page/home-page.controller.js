b0b2bf127b5991ac9f7bd708c53de6469a3bf527
Ahmed Alaa <ahmed.3laa.2020@gmail.com>
2022-06-14 20:48:34 +0200

    feat(load_more): implement load more employees

--- app/pages/home-page/home-page.controller.js
+++ app/pages/home-page/home-page.controller.js
@@ -14,10 +14,34 @@ function homePageController(Employees, $location) {
     homePageVm.filterQueryString = $location.search().filter;
   }
   activate();
+  // load more config
+  homePageVm.page = null;
+  homePageVm.isLoading = false;
+  homePageVm.totalPages = null;
+  homePageVm.handleLoadMoreEmployees = function () {
+    homePageVm.isLoading = true;
+    loadMoreEmployees(homePageVm.page + 1);
+  };
   function activate() {
     Employees.getEmployees()
       .then(({ data }) => {
         homePageVm.employees = homePageVm.employees.concat(data.employees);
+        homePageVm.page = data.current_page;
+        homePageVm.totalPages = data.pages;
+        homePageVm.isLoading = false;
+        homePageVm.isMoreData = false;
+      }).catch(err=>console.log('Server err', err));
+  }
+  function loadMoreEmployees(page) {
+    Employees.loadMoreEmployees(page)
+      .then(({ data }) => {
+        homePageVm.page = data.current_page;
+        homePageVm.totalPages = data.pages;
+        homePageVm.employees = homePageVm.employees.concat(data.employees);
+        homePageVm.isLoading = false;
+        if (data.current_page === data.pages) {
+          homePageVm.isMoreData = true;
+        }
       });
   }
 }
