df1ed46ca7ab4fdd59fbff5d6f970d1d3475b68c
Ahmed Alaa <Ahmed.3laa.2020@gmail.com>
2022-06-14 20:50:12 +0200

    feat(load_more): implement load more employees (#5)

--- app/services/employees.service.js
+++ app/services/employees.service.js
@@ -3,12 +3,12 @@ angular.module('appModule').service('Employees', EmployeesService);
 function EmployeesService($http) {
   const getEmployees = () => {
     const employeesUrl = 'https://fe-task.getsandbox.com/employees';
-
     return $http.get(employeesUrl);
   };
 
-  const loadMoreEmployees = () => {
-    // [Load more empolyess logic goes here]
+  const loadMoreEmployees = (page) => {
+    const employeesUrl = `https://fe-task.getsandbox.com/employees?page=${page}`;
+    return $http.get(employeesUrl);
   };
 
   return {
